9626425c62da0c4af4efa1a9e284c55f2fac114a
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-15 12:00:35 -0300

    create interface list

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 15/12/19.
+//  Copyright © 2019 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @objc func addButton(){
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+
+}
