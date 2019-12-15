9dba249cbe3820b1905a2e34317478ade7d0f789
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-15 12:16:22 -0300

    show data in table

--- TravelBook/ListViewController.swift
+++ TravelBook/ListViewController.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var titleArray = [String]()
+    var idArray = [UUID]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
     navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getData()
+    }
+    
+    @objc func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Place")
+        request.returnsObjectsAsFaults = false
+        do{
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                for result in results as! [NSManagedObject] {
+                    if let title = result.value(forKey: "title") as? String {
+                        self.titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    
+                }
+                tableView.reloadData()
+            }
+        }catch{
+            print(error.localizedDescription)
+        }
     }
     
     @objc func addButton(){
@@ -24,12 +55,12 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
 
